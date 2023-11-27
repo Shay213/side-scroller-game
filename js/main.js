b9f34328c7e820378136b8b73b4ab45446444124
shay213 <dawidpawelec@o2.pl>
2023-11-27 15:59:41 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let canvas = null
+let ctx = null
+
+window.addEventListener('load', () => {
+  canvas = document.getElementById('canvas')
+  ctx = canvas.getContext('2d')
+  canvas.width = 800
+  canvas.height = 720
+})
+
+function animate(){
+
+}
+
+function handleEnemies(){
+
+}
+
+function displayStatusText(){
+  
+}
