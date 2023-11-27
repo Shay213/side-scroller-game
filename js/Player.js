ceb91eb1af311b1f376c61a33778d7b9b93da723
shay213 <dawidpawelec@o2.pl>
2023-11-27 16:20:43 +0100

    Add player update and draw methods (move rect to the right)

--- js/Player.js
+++ js/Player.js
@@ -1,3 +1,19 @@
 export default class Player {
-  
+  constructor(gameWidth, gameHeight) {
+    this.gameWidth = gameWidth
+    this.gameHeight = gameHeight
+    this.width = 200
+    this.height = 200
+    this.x = 0
+    this.y = this.gameHeight - this.height
+  }
+
+  update(){
+    this.x++
+  }
+
+  draw(ctx){
+    ctx.fillStyle = 'white'
+    ctx.fillRect(this.x, this.y, this.width, this.height)
+  }
 }
--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
 import InputHandler from "./InputHandler.js"
-
-let canvas = null
-let ctx = null
+import Player from "./Player.js"
 
 window.addEventListener('load', () => {
-  canvas = document.getElementById('canvas')
-  ctx = canvas.getContext('2d')
+  const canvas = document.getElementById('canvas')
+  const ctx = canvas.getContext('2d')
   canvas.width = 800
   canvas.height = 720
 
   const input = new InputHandler()
+  const player = new Player(canvas.width, canvas.height)
+  player.draw(ctx)
+
+  const animate = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    player.update()
+    player.draw(ctx)
+
+    requestAnimationFrame(animate)
+  }
+
+  animate()
 })
-
-function animate(){
-
-}
 
 function handleEnemies(){
 
